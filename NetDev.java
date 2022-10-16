1661a40757cc2c88052b463fb8c7fac9b8ff0e40
takahiro-aoki494 <arikatuki@gmail.com>
2022-10-16 17:09:44 +0900

    実行結果の表示に一部線を追加

--- NetDev.java
+++ NetDev.java
@@ -2,18 +2,29 @@ import java.util.List;
 
 public class NetDev {
     public static void main(String[] args) {
-        List<Device> DeviceList = List.of(new Device("Catalyst 3650 シリーズ ", "Cisco", "Switch"), new Device("Catalyst 9200 シリーズ ", "Cisco", "Switch"), new Device("Catalyst 8300 シリーズ ", "Cisco", "Router"), new Device("ASR 9000 シリーズ ", "Cisco", "Router"), new Device("EX4100 シリーズ ", "Juniper", "Switch"), new Device("EX3400 シリーズ ", "Juniper", "Switch"), new Device("MX10004 シリーズ ", "Juniper", "Router"), new Device("MX960 シリーズ ", "Juniper", "Router"));
+        //ネットワーク機器の情報を格納したリストを作成
+        List<Device> DeviceList = List.of(
+                new Device("Catalyst 3650 シリーズ ", "Cisco", "Switch"),
+                new Device("Catalyst 9200 シリーズ ", "Cisco", "Switch"),
+                new Device("Catalyst 8300 シリーズ ", "Cisco", "Router"),
+                new Device("ASR 9000 シリーズ ", "Cisco", "Router"),
+                new Device("EX4100 シリーズ ", "Juniper", "Switch"),
+                new Device("EX3400 シリーズ ", "Juniper", "Switch"),
+                new Device("MX10004 シリーズ ", "Juniper", "Router"),
+                new Device("MX960 シリーズ ", "Juniper", "Router"));
 
         System.out.println("ネットワーク機器一覧");
+        System.out.println("----------------------------------");
 
         //リストの内容をすべて表示
         for (Device device : DeviceList) {
             System.out.println(device.getName());
             System.out.print("ベンダー：" + device.getVendor());
             System.out.println(" 種類：" + device.getType());
-
+            System.out.println("----------------------------------");
         }
 
+        System.out.println("");
         System.out.println("Ciscoのスイッチのみ表示");
 
         //ベンダーがCiscoかつタイプがSwitchの機器のみ表示
@@ -21,7 +32,6 @@ public class NetDev {
             if (device.getVendor().equals("Cisco") && device.getType().equals("Switch")) {
                 System.out.println(device.getName());
             }
-
         }
     }
 }
